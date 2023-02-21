51dd4090d576ca9d4c069579a2900d40d2faf738
Archisman <archisman.mukherjee02@gmail.com>
2023-02-22 01:51:15 +0530

    Converting to AJAX(Creating a post: Sending data)

--- controllers/comments_controller.js
+++ controllers/comments_controller.js
@@ -12,14 +12,16 @@ module.exports.create = async function (req, res) {
         user: req.user._id,
       });
 
+      req.flash("success", "Comment added!");
+
       post.comments.push(comment);
       post.save();
 
       res.redirect("/");
     }
   } catch (err) {
-    console.log("Error", err);
-    return;
+    req.flash("error", "Comment cannot be added!");
+    return res.redirect("back");
   }
 };
 
@@ -32,16 +34,19 @@ module.exports.destroy = async function (req, res) {
 
       comment.remove();
 
+      req.flash("success", "Comment removed!");
+
       let post = await Post.findByIdAndUpdate(postId, {
         $pull: { comments: req.params.id },
       });
 
       return res.redirect("back");
     } else {
+      req.flash("error", "Comment cannot be removed!");
       return res.redirect("back");
     }
   } catch (err) {
-    console.log("Error", err);
-    return;
+    req.flash("error", "Comment cannot be removed!");
+    return res.redirect("back");
   }
 };
--- controllers/posts_controller.js
+++ controllers/posts_controller.js
@@ -4,15 +4,26 @@ const Comment = require("../models/comment");
 module.exports.create = async function (req, res) {
   // return res.send("<h1>Hi you can make a post!</h1>");
   try {
-    await Post.create({
+    let post = await Post.create({
       content: req.body.content,
       user: req.user._id,
     });
 
+    // check if the request is AJAX request
+    if (req.xhr) {
+      return res.status(200).json({
+        data: {
+          post: post,
+        },
+        message: "Post Created!",
+      });
+    }
+
+    req.flash("success", "Post published!");
     return res.redirect("back");
   } catch (err) {
-    console.log("Error", err);
-    return;
+    req.flash("error", err);
+    return res.redirect("back");
   }
 };
 
@@ -25,12 +36,16 @@ module.exports.destroy = async function (req, res) {
       post.remove();
 
       await Comment.deleteMany({ post: req.params.id });
+
+      req.flash("success", "Post and associated comments deleted!");
+
       return res.redirect("back");
     } else {
+      req.flash("error", "You cannot delete this post!");
       return res.redirect("back");
     }
   } catch (err) {
-    console.log("Error", err);
-    return;
+    req.flash("error", err);
+    return res.redirect("back");
   }
 };
