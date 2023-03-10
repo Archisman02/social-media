9541271a9417082e0bef3e50664e193c313c7723
Archisman <archisman.mukherjee02@gmail.com>
2023-03-10 21:55:10 +0530

    Setting up kue and using it to handle delayed jobs

--- mailers/comments_mailer.js
+++ mailers/comments_mailer.js
@@ -2,14 +2,19 @@ const nodeMailer = require("../config/nodemailer");
 
 // this is another way of exporting a method (instead of module.exports)
 exports.newComment = (comment) => {
-  console.log("inside newComment mailer");
+  let htmlString = nodeMailer.renderTemplate(
+    {
+      comment: comment,
+    },
+    "/comments/new_comment.ejs"
+  );
 
   nodeMailer.transporter.sendMail(
     {
       from: "archisman.mukherjee02@gmail.com",
       to: comment.user.email,
       subject: "New Comment Published",
-      html: "<h1>Yup, your comment is now published!</h1>",
+      html: htmlString,
     },
     (err, info) => {
       if (err) {
@@ -17,7 +22,7 @@ exports.newComment = (comment) => {
         return;
       }
 
-      console.log("Message sent", info);
+      // console.log("Message sent", info);
       return;
     }
   );
